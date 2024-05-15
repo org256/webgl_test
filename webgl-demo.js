affd2ce49b5b136c1148f205ed3fb3fb7d3b6fd6
org256 <93014680+org256@users.noreply.github.com>
2024-05-16 11:22:08 +1200

    https://github.com/mdn/dom-examples/blob/main/webgl-examples/tutorial/sample4

--- webgl-demo.js
+++ webgl-demo.js
@@ -1,6 +1,9 @@
 import { initBuffers } from "./init-buffers.js";
 import { drawScene } from "./draw-scene.js";
 
+let squareRotation = 0.0;
+let deltaTime = 0;
+
 main();
 
 //
@@ -78,8 +81,20 @@ function main() {
   // objects we'll be drawing.
   const buffers = initBuffers(gl);
 
-  // Draw the scene
-  drawScene(gl, programInfo, buffers);
+  let then = 0;
+
+  // Draw the scene repeatedly
+  function render(now) {
+    now *= 0.001; // convert to seconds
+    deltaTime = now - then;
+    then = now;
+
+    drawScene(gl, programInfo, buffers, squareRotation);
+    squareRotation += deltaTime;
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
 
 //
